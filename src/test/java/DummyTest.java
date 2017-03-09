d23d4686755eb295311b234cbb17f9213ceb8f11
Verschueren,Berry B.C.H <b.verschueren@student.fontys.nl>
2017-03-09 14:20:51 +0100

    Dummy test added for jenkins testing purposes.

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Berry-PC on 09/03/2017.
+ */
+public class DummyTest {
+    @Test
+    public void test() {
+        boolean x = true;
+        assertEquals(false, x);
+    }
+}
