f3f0f8980f777129d97a96670f9cffd8034ef4bf
Verschueren,Berry B.C.H <b.verschueren@student.fontys.nl>
2017-03-10 15:55:08 +0100

    Working DB (resource local)!! -> implement jpa dao impl

--- src/main/java/dao/implementations/jpa/HashtagDAOImpJPA.java
+++ src/main/java/dao/implementations/jpa/HashtagDAOImpJPA.java
@@ -5,6 +5,9 @@ import model.Hashtag;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Alternative;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 /**
@@ -13,18 +16,60 @@ import java.util.List;
 @RequestScoped
 @Alternative
 public class HashtagDAOImpJPA implements HashtagDAO {
+
+    private static final String PERSISTENCE_UNIT_NAME = "kwetterDB";
+    private static EntityManagerFactory factory  = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    private EntityManager em = factory.createEntityManager();
+
+    public HashtagDAOImpJPA() {}
+
     @Override
     public Hashtag save(Hashtag hashtag) {
+        if (hashtag == null || hashtag.getInhoud() == null || hashtag.getInhoud().isEmpty())
+            return null;
+
+        try {
+            em.getTransaction().begin();
+            em.persist(hashtag);
+            em.getTransaction().commit();
+        }
+        catch (Exception x) {
+            em.getTransaction().rollback();
+            return null;
+        }
+
         return null;
     }
 
     @Override
     public boolean delete(long id) {
+        if (id >= 0) {
+            try {
+                em.getTransaction().begin();
+                Hashtag hashtag = get(id);
+                em.remove(hashtag);
+                em.getTransaction().commit();
+            }
+            catch (Exception x) {
+                em.getTransaction().rollback();
+                return false;
+            }
+        }
+
         return false;
     }
 
     @Override
     public Hashtag get(long id) {
+        if (id >= 0) {
+            try {
+                return em.find(Hashtag.class, id);
+            }
+            catch (Exception x) {
+                return null;
+            }
+        }
+
         return null;
     }
 
