363044e6c18c02e556668344ff45f79b20ebe0fc
Verschueren,Berry B.C.H <b.verschueren@student.fontys.nl>
2017-03-14 10:52:49 +0100

    trying to fix logout endpoint

--- src/main/java/controller/TestAPI.java
+++ src/main/java/controller/TestAPI.java
@@ -1,11 +1,16 @@
 package controller;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.SecurityContext;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import java.net.URI;
 
 /**
  * Created by Berry-PC on 09/03/2017.
@@ -16,25 +21,36 @@ import javax.ws.rs.core.Context;
 public class TestAPI {
 
     @GET
-    @Path("/contexthello")
+    @Path("/xmlbye")
     @Produces("text/plain;charset=UTF-8")
-    public String sayHello(@Context SecurityContext sc) {
-        if (sc.isUserInRole("admin")) return "HELLO WORLD!";
-        throw new SecurityException("User is unauthorized.");
-    }
+    public Response sayBye(@Context HttpServletRequest request, @Context Response response) {
+        try {
+            HttpSession session = request.getSession();
+            session.invalidate();
+            request.logout();
+            session.invalidate();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
 
-    @GET
-    @Path("/annotatedhello")
-    @Produces("text/plain;charset=UTF-8")
-    @RolesAllowed("admin")
-    public String sayHelloAnnotated() {
-        return "HELLO WORLD!";
+        URI uri = URI.create(request.getContextPath() + "/rest/kwetteraar/get/more");
+
+        return Response.seeOther(uri).build();
     }
 
     @GET
     @Path("/xmlhello")
     @Produces("text/plain;charset=UTF-8")
-    public String sayHelloXml() {
-        return "HELLO WORLD!";
+    public Response sayHelloXml(@Context HttpServletRequest request, @Context Response response) {
+        CacheControl cc = new CacheControl();
+        cc.setNoCache(true);
+
+        String username = request.getRemoteUser();
+        String returnString = "HELLO WORLD! Welcome, " + username;
+
+        ResponseBuilder builder = Response.ok(returnString);
+        builder.cacheControl(cc);
+
+        return builder.build();
     }
 }
